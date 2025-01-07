42050b3f14ef23875c754c035a678a2fde32a527
ImShubham09 <singhashubham612@gmail.com>
2025-01-07 15:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //your JS code here. If required.
+
+// Dynamically move the script to the head to avoid it being a body child
+if (document.currentScript) {
+    document.head.appendChild(document.currentScript);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     // Create a new text node with the string "DOM load success"
     const textNode = document.createTextNode("DOM load success");
